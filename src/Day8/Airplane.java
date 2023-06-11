7066a356d2e52e3a996d3f83a43e6fbe7a075c9b
TrukhinMI <max.trukhin93@gmail.com>
2023-06-11 21:53:19 +0300

    add day8

---
+++ src/Day8/Airplane.java
@@ -0,0 +1,58 @@
+package Day8;
+
+public class Airplane {
+    private String manufacturer; //(изготовитель)
+    private int year; //(год выпуска)
+    private double length; //(длина)
+    private double weight; //(вес)
+    private double fuel; //(количество топлива в баке)
+
+    public Airplane(String manufacturer, int year, double length, double weight) {
+        this.manufacturer = manufacturer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+        this.fuel = 0;
+    }
+
+    public void info() {
+        System.out.println("Изготовитель: " + manufacturer + ", год выпуска: " + year + ", длина: " + length + ", вес: " + weight + ", количество топлива в баке: " + fuel);
+    }
+
+    @Override
+    public String toString() {
+        return "Изготовитель: " + manufacturer + ", год выпуска: " + year + ", длина: " + length + ", вес: " + weight + ", количество топлива в баке: " + fuel;
+    }
+
+    public void fillUp(int n){
+        fuel = fuel + n;
+    }
+
+
+
+
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+}
